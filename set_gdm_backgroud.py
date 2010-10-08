14871846f590523ab1d5dfa79d1a639f2e8101d9
mauro <maurotorrez@gmail.com>
2010-10-08 00:49:35 -0300

    Agrego GPL notice.

--- set_gdm_backgroud.py
+++ set_gdm_backgroud.py
@@ -1,5 +1,20 @@
-#!/usr/bin/env python2.6
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2010 Mauro Torrez. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import re
@@ -253,8 +268,11 @@ class App:
       self.combo.append_text("wallpaper")
       self.combo.connect( "changed", self.disp_changed)
 
+      i=0
+      self.combo.set_active(i)
       while self.combo.get_active_text() != self.disp:
-         self.combo.set_active( self.combo.get_active() + 1 )
+         i+=1
+         self.combo.set_active( i )
          
       self.filedialog.set_filename(self.path)
       
